82d3cb4e64f2d84654500b2462a597f12eeabbbf
sginko <s.ginkota@gmail.com>
2024-04-23 16:25:10 +0200

    some changes

--- src/main/java/pl/akademiaspecjalistowit/jokeappspring/joke/repository/InMemoryJokeRepository.java
+++ src/main/java/pl/akademiaspecjalistowit/jokeappspring/joke/repository/InMemoryJokeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import pl.akademiaspecjalistowit.jokeappspring.joke.model.Joke;
 
@@ -14,22 +15,22 @@ public class InMemoryJokeRepository implements JokeRepository {
 
     public InMemoryJokeRepository() {
         this.jokes = List.of(
-            new Joke("What is an object-oriented way to make a fortune? Inheritance",
-                "Programming"),
-            new Joke("What does an IT specialist say when he receives a pendrive for " +
-                "his birthday? Thanks for remembering",
-                "Programming"),
-            new Joke("What is the difference between programmers and politicians? " +
-                "Programmers are only paid for programs that work",
-                "Politics"),
-            new Joke("How can you tell if a politician is lying? By the way he moves his mouth...",
-                "Politics"),
-            new Joke("How on earth did you manage to become a professional bodybuilder " +
-                "when you were always a loser in Physical Education? Because I was always a top student in chemistry",
-                "Sport"),
-            new Joke("MY JOKE Why does a Polish national team fan need a scarf? " +
-                "So that he would have something to wipe his tears with",
-                "Programming"));
+                new Joke("What is an object-oriented way to make a fortune? Inheritance",
+                        "My"),
+                new Joke("What does an IT specialist say when he receives a pendrive for " +
+                        "his birthday? Thanks for remembering",
+                        "My"),
+                new Joke("What is the difference between programmers and politicians? " +
+                        "Programmers are only paid for programs that work",
+                        "My"),
+                new Joke("How can you tell if a politician is lying? By the way he moves his mouth...",
+                        "MY"),
+                new Joke("How on earth did you manage to become a professional bodybuilder " +
+                        "when you were always a loser in Physical Education? Because I was always a top student in chemistry",
+                        "My"),
+                new Joke("MY JOKE Why does a Polish national team fan need a scarf? " +
+                        "So that he would have something to wipe his tears with",
+                        "Programming"));
     }
 
     @Override
@@ -40,9 +41,9 @@ public class InMemoryJokeRepository implements JokeRepository {
     @Override
     public List<Joke> getAllByCategory(String category) {
         return jokes
-            .stream()
-            .filter(compareCategories(category))
-            .collect(Collectors.toList());
+                .stream()
+                .filter(compareCategories(category))
+                .collect(Collectors.toList());
     }
 
     private static Predicate<Joke> compareCategories(String category) {
